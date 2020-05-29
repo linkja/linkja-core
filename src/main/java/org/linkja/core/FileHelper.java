5ab34c4ccec477c5ebe62ca73a3ad62b6780ee1f
Luke Rasmussen <luke.rasmussen@gmail.com>
2020-05-29 15:04:07 -0500

    Refactor binary file read/write routeins to FileHelper

--- src/main/java/org/linkja/core/FileHelper.java
+++ src/main/java/org/linkja/core/FileHelper.java
@@ -1,7 +1,7 @@
 package org.linkja.core;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,5 +21,76 @@ public class FileHelper {
 
   public Path pathFromString(String path) {
     return Paths.get(path);
+  }
+
+
+  /**
+   * Writes the 32-bit int to the binary output stream.
+   * @param x the {@code int} to write
+   */
+  public static void writeInt(BufferedOutputStream stream, int x) throws IOException {
+    for (int index = Integer.BYTES - 1; index >= 0; index--) {
+      stream.write((byte)((x >>> (8 * index)) & 0xFF));
+    }
+  }
+
+  /**
+   * Writes the 64-bit long to the binary output stream.
+   * @param x the {@code long} to write
+   */
+  public static void writeLong(BufferedOutputStream stream, long x) throws IOException {
+    for (int index = Long.BYTES - 1; index >= 0; index--) {
+      stream.write((byte)((x >>> (8 * index)) & 0xFF));
+    }
+  }
+
+  /**
+   * Writes the 64-bit long to the binary output stream.
+   * @param x the {@code long} to write
+   */
+  public static void writeLong(RandomAccessFile file, long x) throws IOException {
+    for (int index = Long.BYTES - 1; index >= 0; index--) {
+      file.write((byte)((x >>> (8 * index)) & 0xFF));
+    }
+  }
+
+
+  /**
+   * Reads the 32-bit int from the binary input stream.
+   * @param stream the {@code BufferedInputStream} to read from
+   * @param exceptionMessage a descriptive exception message to throw if reading fails
+   * @return the 32-bit int
+   * @throws LinkjaException
+   * @throws IOException
+   */
+  public static int readInt(BufferedInputStream stream, String exceptionMessage) throws LinkjaException, IOException {
+    byte[] buffer = new byte[Integer.BYTES];
+    int result = stream.read(buffer, 0, Integer.BYTES);
+    if (result != Integer.BYTES) {
+      throw new LinkjaException(exceptionMessage);
+    }
+
+    int value = ByteBuffer.wrap(buffer, 0, Integer.BYTES).getInt();
+    return value;
+  }
+
+  /**
+   * Reads the 64-bit long from the binary input stream.
+   * @param stream the {@code BufferedInputStream} to read from
+   * @param exceptionMessage a descriptive exception message to throw if reading fails
+   * @return the 64-bit long
+   * @throws LinkjaException
+   * @throws IOException
+   */
+  public static long readLong(BufferedInputStream stream, String exceptionMessage) throws LinkjaException, IOException {
+    byte[] buffer = new byte[Long.BYTES];
+
+    int result = stream.read(buffer, 0, Long.BYTES);
+    if (result != Long.BYTES) {
+      throw new LinkjaException(exceptionMessage);
+    }
+
+    long value = ByteBuffer.wrap(buffer, 0, Long.BYTES).getLong();
+    return value;
   }
 }
